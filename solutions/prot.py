9402eaf8c1fa22542b6ff38a599bbb550a2b14fb
Raymond House <raymondhouse2003@gmail.com>
2023-09-25 00:50:39 -0700

    Create prot.py

---
+++ solutions/prot.py
@@ -0,0 +1,38 @@
+with open('rosalind_prot.txt', 'r') as f:
+    contents = f.read()
+
+codon_dict = {
+    "UUU": "F", "CUU": "L", "AUU": "I", "GUU": "V",
+    "UUC": "F", "CUC": "L", "AUC": "I", "GUC": "V",
+    "UUA": "L", "CUA": "L", "AUA": "I", "GUA": "V",
+    "UUG": "L", "CUG": "L", "AUG": "M", "GUG": "V",
+    "UCU": "S", "CCU": "P", "ACU": "T", "GCU": "A",
+    "UCC": "S", "CCC": "P", "ACC": "T", "GCC": "A",
+    "UCA": "S", "CCA": "P", "ACA": "T", "GCA": "A",
+    "UCG": "S", "CCG": "P", "ACG": "T", "GCG": "A",
+    "UAU": "Y", "CAU": "H", "AAU": "N", "GAU": "D",
+    "UAC": "Y", "CAC": "H", "AAC": "N", "GAC": "D",
+    "UAA": "Stop", "CAA": "Q", "AAA": "K", "GAA": "E",
+    "UAG": "Stop", "CAG": "Q", "AAG": "K", "GAG": "E",
+    "UGU": "C", "CGU": "R", "AGU": "S", "GGU": "G",
+    "UGC": "C", "CGC": "R", "AGC": "S", "GGC": "G",
+    "UGA": "Stop", "CGA": "R", "AGA": "R", "GGA": "G",
+    "UGG": "W", "CGG": "R", "AGG": "R", "GGG": "G"
+}
+
+op = ""
+
+idx = 0
+while idx < len(contents):
+    print(contents[idx:idx+3])
+    codon = contents[idx:idx+3]
+    amino_acid = codon_dict.get(codon)
+    if amino_acid is None:
+        print(f"Warning: Codon {codon} not found in codon_dict")
+        break
+    elif amino_acid == "Stop":
+        break
+    op += amino_acid
+    idx += 3
+
+print(op)
